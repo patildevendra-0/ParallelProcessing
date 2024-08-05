56fdec471bb549588eed55abde81536c4031098b
patildevendra-0 <patildev2001@gmail.com>
2024-08-05 21:20:11 +0530

    V1.1

---
+++ ParallelProcessing_Basics/Parallelism.c
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------------------------------------//
+/*                                               Parallelism
+//------------------------------------------------------------------------------------------------------------//
+
+        # PROGRAM : 
+                        Program is a set of instructions.
+
+        # Instructions : 
+                         In computing, an instruction is a single operation executed by a computer's processor (CPU) as part of a program. 
+                         Instructions tell the computer what actions to perform and are fundamental building blocks of a program. 
+                         Each instruction is a binary code that the CPU can decode and execute.
+
+        # Processor :
+                         It executes instructions from programs, performing basic arithmetic, logic, control, and input/output (I/O) operations 
+                         specified by the instructions                 
+
+
+
+
+*/
+//------------------------------------------------------------------------------------------------------------//
